84fe7411c2aa7310e1bd67ea27ac231142fc14c1
Jedidah Roberts <robertsjedidah@gmail.com>
2025-09-08 13:09:12 +0000

    updated experiences

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -25,26 +25,46 @@ const Experience = () => {
         {/* Experience Timeline */}
         <div className="space-y-10">
           
-          {/* Experience Item */}
+          {/*  Item 1*/}
+           <motion.div
+            className="bg-white shadow-lg rounded-xl p-6 border-l-4 border-orange-500"
+            initial={{ opacity: 0, x: -30 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.7 }}
+          >
+            <span className="text-sm text-gray-500 font-medium block mb-2">July 2025 – August 2025</span>
+            <h3 className="text-xl font-semibold text-gray-800 mb-2">
+              Web Development Intern – Beyond the Line Africa
+            </h3>
+            <ul className="list-disc list-inside text-gray-700 leading-relaxed space-y-1">
+              <li>Worked as part of a collaborative development team,building real-world web products using React.js and Tailwind CSS</li>
+              <li>Contributed to the development of user-facing features, responsive UI components, and seamless user experiences</li>
+              <li>Gained hands-on experiences working in a real -world development environment with emphasis on clean,maintainable code </li>
+              
+              
+            </ul>
+          </motion.div>
+          {/* Item 2 */}
           <motion.div
             className="bg-white shadow-lg rounded-xl p-6 border-l-4 border-orange-500"
             initial={{ opacity: 0, x: -30 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.7 }}
           >
-            <span className="text-sm text-gray-500 font-medium block mb-2">May 2025 – Present</span>
+            <span className="text-sm text-gray-500 font-medium block mb-2">May 2025 – July 2025</span>
             <h3 className="text-xl font-semibold text-gray-800 mb-2">
               Web Development Trainee – GROW's Web Development Program
             </h3>
             <ul className="list-disc list-inside text-gray-700 leading-relaxed space-y-1">
-              <li>Built a personal portfolio from scratch using HTML and CSS.</li>
-              <li>Features sections like About Me, Projects, and Contact.</li>
-              <li>Applies version control using Git and GitHub to manage and document progress.</li>
+              <li>Built a personal portfolio from scratch using React and Tailwind CSS</li>
               <li>Collaborated with fellow learners on small projects using clean, structured code.</li>
+              <li>Collaborated in groups to ceate big projects, focusing on API integrations and reusable components.</li>
+              <li>Applied version control using Git and GitHub to manage and document progress.</li>
+              
             </ul>
           </motion.div>
 
-          {/* Experience Item */}
+          {/*  Item 3*/}
           <motion.div
             className="bg-white shadow-lg rounded-xl p-6 border-l-4 border-orange-500"
             initial={{ opacity: 0, x: -30 }}
@@ -62,7 +82,7 @@ const Experience = () => {
             </ul>
           </motion.div>
 
-          {/* Experience Item */}
+          {/* Item 4 */}
           <motion.div
             className="bg-white shadow-lg rounded-xl p-6 border-l-4 border-orange-500"
            initial={{ opacity: 0, x: -30 }}
